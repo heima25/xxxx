89b5fe8e1e193a37005bedac71308527f931b4e7
HM <HM@HMMacs.local>
2017-06-01 18:06:57 +0800

    把食物放网上!

---
+++ helldd/helldd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helldd
+//
+//  Created by HM on 2017/5/28.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helldd/helldd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  helldd
+//
+//  Created by HM on 2017/5/28.
+//  Copyright © 2017年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
